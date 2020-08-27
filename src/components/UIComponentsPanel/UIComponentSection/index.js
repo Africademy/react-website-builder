ce6a1c44f0ebd05d478dc61a41ccb4a2a33985be
yash11213018 <52713940+yash11213018@users.noreply.github.com>
2020-08-27 21:42:44 +0530

    basic drag-drop fix able to parse data of the dropped components.

--- src/components/UIComponentsPanel/UIComponentSection/index.js
+++ src/components/UIComponentsPanel/UIComponentSection/index.js
@@ -1,48 +1,54 @@
-import React from 'react'
-import ComponentButton from './ComponentButton'
-import SectionCollapseButton from './SectionCollapseButton'
-import * as  ReactBootstrap from 'react-bootstrap'
-import './UIComponentSection.css'
-
+import React from "react";
+import SectionCollapseButton from "./SectionCollapseButton";
+import * as ReactBootstrap from "react-bootstrap";
+import "./UIComponentSection.css";
+import Elements from "./Elements";
+import Components from "./Components";
 class UIComponentSection extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { isExpanded: true }
-        this.handleExpansion = this.handleExpansion.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = { isExpanded: true };
+    this.handleExpansion = this.handleExpansion.bind(this);
+  }
+
+  handleExpansion(e) {
+    this.setState((state) => ({ isExpanded: !state.isExpanded }));
+  }
+
+  render() {
+    const rows = [];
+    const values = this.props.values;
+
+    const types = {
+      elements: Elements,
+      components: Components
+    };
+
+    for (let vals in values) {
+      if (values.hasOwnProperty(vals)) {
+        const Name = types[this.props.name];
+        rows.push(
+          <ReactBootstrap.Row key={vals} className="justify-content-center">
+            <Name name={vals} attr={values[vals]} />
+          </ReactBootstrap.Row>
+        );
+      }
     }
 
-    handleExpansion(e) {
-        this.setState((state) => ({ isExpanded: !state.isExpanded }));
-    }
-
-    render() {
-        const rows = []
-        
-        var keys = [];
-        for (let key in this.props.values) {
-            if (this.props.values.hasOwnProperty(key)) keys.push(key);
-        }
-        const len = keys.length;
-
-        for (var i = 0; i < len / 3; i++) {
-            const row = []
-            for (var j = i * 3, k = 0; j < len && k < 3; j++, k++){
-                row.push(<ComponentButton key={j} name={keys[j]} attr={this.props.values[keys[j]]}></ComponentButton>);
-            }
-            rows.push(<ReactBootstrap.Row key={i} className="justify-content-center">{row}</ReactBootstrap.Row>);
-        }
-
-        return (
-            <div className="section">
-                <SectionCollapseButton name={this.props.name}
-                    onClick={this.handleExpansion}
-                />
-                <ReactBootstrap.Collapse in={this.state.isExpanded}>
-                    <ReactBootstrap.Container id={this.props.name}>{rows}</ReactBootstrap.Container>
-                </ReactBootstrap.Collapse>
-            </div>
-        )
-    }
+    return (
+      <div className="section">
+        <SectionCollapseButton
+          name={this.props.name}
+          onClick={this.handleExpansion}
+        />
+        <ReactBootstrap.Collapse in={this.state.isExpanded}>
+          <ReactBootstrap.Container id={this.props.name}>
+            {rows}
+          </ReactBootstrap.Container>
+        </ReactBootstrap.Collapse>
+      </div>
+    );
+  }
 }
 
 export default UIComponentSection;
--- src/components/WebPreviewArea/index.js
+++ src/components/WebPreviewArea/index.js
@@ -1,50 +1,52 @@
-import React from 'react'
-import './WebPreviewArea.css'
+import React from "react";
+import "./WebPreviewArea.css";
 
 class WebPreviewArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.dragover_handler = this.dragover_handler.bind(this);
+    this.drop_handler = this.drop_handler.bind(this);
+    this.on_click = this.on_click.bind(this);
+    this.dropped = "";
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { data: [] };
-        this.dragover_handler = this.dragover_handler.bind(this);
-        this.drop_handler = this.drop_handler.bind(this);
-        this.on_click = this.on_click.bind(this);
-        this.dropped = "";
-    }
+  on_click(e, data) {
+    console.log(e.target);
+    console.log(data);
+  }
 
-    on_click(e,data){
-        console.log(e.target);
-        console.log(data);
-    }
+  drop_handler(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("attributes");
+    data = JSON.parse(data);
+    var ele = React.createElement(
+      data.tag,
+      { ...data, onClick: this.on_click },
+      data.content
+    );
+    this.setState((state, props) => ({
+      data: [...state.data, ele]
+    }));
+  }
 
-    drop_handler(e) {
-        e.preventDefault();
-        var data = e.dataTransfer.getData("text/html");
-        data = JSON.parse(data);
-        var ele = React.createElement(data.name,{...data.attr, onClick: this.on_click},data.val)
-        console.log(typeof this.state.data)
-        this.setState((state,props) => ({
-            data: [...state.data,ele]
-        }));
-    }
+  dragover_handler(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  }
 
-    dragover_handler(e) {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = "move";
-    }
-
-    render() {
-        console.log(this.state)
-        return (
-            (<div id="prev"
-            className="dragArea"
-            onDrop={this.drop_handler}
-            onDragOver={this.dragover_handler}>
-            {this.state.data}
-        </div>
-        )
-        )
-    }
+  render() {
+    return (
+      <div
+        id="prev"
+        className="dragArea"
+        onDrop={this.drop_handler}
+        onDragOver={this.dragover_handler}
+      >
+        {this.state.data}
+      </div>
+    );
+  }
 }
 
 export default WebPreviewArea;
