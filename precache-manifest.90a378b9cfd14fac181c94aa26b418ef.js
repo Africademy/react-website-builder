7a5b6c061e28d9e3d043a6f35918445570976ce1
Dhyey <dhyeyshah4@gmail.com>
2020-08-27 22:26:05 +0530

    Updates

---
+++ precache-manifest.90a378b9cfd14fac181c94aa26b418ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286869ea1c2218782c13da6719e2ed6d",
+    "url": "/react-website-builder/index.html"
+  },
+  {
+    "revision": "a34767748da57a0b7ced",
+    "url": "/react-website-builder/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c35e6088ec11628662f9",
+    "url": "/react-website-builder/static/css/main.456c9c69.chunk.css"
+  },
+  {
+    "revision": "a34767748da57a0b7ced",
+    "url": "/react-website-builder/static/js/2.34747676.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-website-builder/static/js/2.34747676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35e6088ec11628662f9",
+    "url": "/react-website-builder/static/js/main.a34452db.chunk.js"
+  },
+  {
+    "revision": "7e5f01fd2a9da9f89641",
+    "url": "/react-website-builder/static/js/runtime-main.0c5d46cc.js"
+  }
+]);
